7bedbcebf7383231315d5f07901c24fc5924d339
anubhavshrimal <anubhavshrimal@gmail.com>
2017-06-09 17:43:24 +0530

    Added session 1 notebook for calculating mean, standard deviation and normalizing an image

---
+++ Practice/session-1/libs/utils.py
@@ -0,0 +1,268 @@
+"""Utilities used in the Kadenze Academy Course on Deep Learning w/ Tensorflow.
+
+Creative Applications of Deep Learning w/ Tensorflow.
+Kadenze, Inc.
+Parag K. Mital
+
+Copyright Parag K. Mital, June 2016.
+"""
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import urllib
+import numpy as np
+import zipfile
+import os
+from scipy.misc import imsave
+
+
+def imcrop_tosquare(img):
+    """Make any image a square image.
+    Parameters
+    ----------
+    img : np.ndarray
+        Input image to crop, assumed at least 2d.
+    Returns
+    -------
+    crop : np.ndarray
+        Cropped image.
+    """
+    size = np.min(img.shape[:2])
+    extra = img.shape[:2] - size
+    crop = img
+    for i in np.flatnonzero(extra):
+        crop = np.take(crop, extra[i] // 2 + np.r_[:size], axis=i)
+    return crop
+
+
+def montage(images, saveto='montage.png'):
+    """Draw all images as a montage separated by 1 pixel borders.
+
+    Also saves the file to the destination specified by `saveto`.
+
+    Parameters
+    ----------
+    images : numpy.ndarray
+        Input array to create montage of.  Array should be:
+        batch x height x width x channels.
+    saveto : str
+        Location to save the resulting montage image.
+
+    Returns
+    -------
+    m : numpy.ndarray
+        Montage image.
+    """
+    if isinstance(images, list):
+        images = np.array(images)
+    img_h = images.shape[1]
+    img_w = images.shape[2]
+    n_plots = int(np.ceil(np.sqrt(images.shape[0])))
+    if len(images.shape) == 4 and images.shape[3] == 3:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1, 3)) * 0.5
+    elif len(images.shape) == 4 and images.shape[3] == 1:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1, 1)) * 0.5
+    elif len(images.shape) == 3:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1)) * 0.5
+    else:
+        raise ValueError('Could not parse image shape of {}'.format(
+            images.shape))
+    for i in range(n_plots):
+        for j in range(n_plots):
+            this_filter = i * n_plots + j
+            if this_filter < images.shape[0]:
+                this_img = images[this_filter]
+                m[1 + i + i * img_h:1 + i + (i + 1) * img_h,
+                  1 + j + j * img_w:1 + j + (j + 1) * img_w] = this_img
+    imsave(arr=np.squeeze(m), name=saveto)
+    return m
+
+
+def get_celeb_files():
+    """Downloads the first 100 images of the celeb dataset.
+
+    Files will be placed in a directory 'img_align_celeba' if one
+    doesn't exist.
+
+    Returns
+    -------
+    files : list of strings
+        Locations to the first 100 images of the celeb net dataset.
+    """
+    # Create a directory
+    if not os.path.exists('img_align_celeba'):
+        os.mkdir('img_align_celeba')
+
+    # Now perform the following 100 times:
+    for img_i in range(1, 101):
+
+        # create a string using the current loop counter
+        f = '000%03d.jpg' % img_i
+
+        if os.path.exists('img_align_celeba/'+f):
+            continue
+
+        # and get the url with that string appended the end
+        url = 'https://s3.amazonaws.com/cadl/celeb-align/' + f
+
+        # We'll print this out to the console so we can see how far we've gone
+        print(url, end='\r')
+
+        # And now download the url to a location inside our new directory
+        urllib.request.urlretrieve(url, os.path.join('img_align_celeba', f))
+
+    files = [os.path.join('img_align_celeba', file_i)
+             for file_i in os.listdir('img_align_celeba')
+             if '.jpg' in file_i]
+    return files
+
+
+def get_celeb_imgs():
+    """Loads the first 100 images of the celeb dataset.
+
+    Returns
+    -------
+    imgs : list of np.ndarray
+        List of the first 100 images from the celeb dataset
+    """
+    return [plt.imread(f_i) for f_i in get_celeb_files()]
+
+
+def gauss(mean, stddev, ksize):
+    """Uses Tensorflow to compute a Gaussian Kernel.
+
+    Parameters
+    ----------
+    mean : float
+        Mean of the Gaussian (e.g. 0.0).
+    stddev : float
+        Standard Deviation of the Gaussian (e.g. 1.0).
+    ksize : int
+        Size of kernel (e.g. 16).
+
+    Returns
+    -------
+    kernel : np.ndarray
+        Computed Gaussian Kernel using Tensorflow.
+    """
+    g = tf.Graph()
+    with tf.Session(graph=g):
+        x = tf.linspace(-3.0, 3.0, ksize)
+        z = (tf.exp(tf.negative(tf.pow(x - mean, 2.0) /
+                           (2.0 * tf.pow(stddev, 2.0)))) *
+             (1.0 / (stddev * tf.sqrt(2.0 * 3.1415))))
+        return z.eval()
+
+
+def gauss2d(mean, stddev, ksize):
+    """Uses Tensorflow to compute a 2D Gaussian Kernel.
+
+    Parameters
+    ----------
+    mean : float
+        Mean of the Gaussian (e.g. 0.0).
+    stddev : float
+        Standard Deviation of the Gaussian (e.g. 1.0).
+    ksize : int
+        Size of kernel (e.g. 16).
+
+    Returns
+    -------
+    kernel : np.ndarray
+        Computed 2D Gaussian Kernel using Tensorflow.
+    """
+    z = gauss(mean, stddev, ksize)
+    g = tf.Graph()
+    with tf.Session(graph=g):
+        z_2d = tf.matmul(tf.reshape(z, [ksize, 1]), tf.reshape(z, [1, ksize]))
+        return z_2d.eval()
+
+
+def convolve(img, kernel):
+    """Uses Tensorflow to convolve a 4D image with a 4D kernel.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        4-dimensional image shaped N x H x W x C
+    kernel : np.ndarray
+        4-dimensional image shape K_H, K_W, C_I, C_O corresponding to the
+        kernel's height and width, the number of input channels, and the
+        number of output channels.  Note that C_I should = C.
+
+    Returns
+    -------
+    result : np.ndarray
+        Convolved result.
+    """
+    g = tf.Graph()
+    with tf.Session(graph=g):
+        convolved = tf.nn.conv2d(img, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        res = convolved.eval()
+    return res
+
+
+def gabor(ksize=32):
+    """Uses Tensorflow to compute a 2D Gabor Kernel.
+
+    Parameters
+    ----------
+    ksize : int, optional
+        Size of kernel.
+
+    Returns
+    -------
+    gabor : np.ndarray
+        Gabor kernel with ksize x ksize dimensions.
+    """
+    g = tf.Graph()
+    with tf.Session(graph=g):
+        z_2d = gauss2d(0.0, 1.0, ksize)
+        ones = tf.ones((1, ksize))
+        ys = tf.sin(tf.linspace(-3.0, 3.0, ksize))
+        ys = tf.reshape(ys, [ksize, 1])
+        wave = tf.matmul(ys, ones)
+        gabor = tf.multiply(wave, z_2d)
+        return gabor.eval()
+
+
+def build_submission(filename, file_list):
+    """Helper utility to check homework assignment submissions and package them.
+
+    Parameters
+    ----------
+    filename : str
+        Output zip file name
+    file_list : tuple
+        Tuple of files to include
+    """
+    # check each file exists
+    for part_i, file_i in enumerate(file_list):
+        assert os.path.exists(file_i), \
+            '\nYou are missing the file {}.  '.format(file_i) + \
+            'It does not look like you have completed Part {}.'.format(
+                part_i + 1)
+
+    # great, each file exists
+    print('It looks like you have completed each part!')
+
+    def zipdir(path, zf):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                # make sure the files are part of the necessary file list
+                if file.endswith(file_list):
+                    zf.write(os.path.join(root, file))
+
+    # create a zip file with the necessary files
+    zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
+    zipdir('.', zipf)
+    zipf.close()
+    print('Great job!!!')
+    print('Now submit the file:\n{}\nto Kadenze for grading!'.format(
+        os.path.abspath(filename)))
