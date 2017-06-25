9efcda321495224e0bcac0c9d962a23739923e66
anubhavshrimal <anubhavshrimal@gmail.com>
2017-06-25 13:20:03 +0530

    Removed saving pickle

--- K_Nearest_Neighbours/knn_sklearn.py
+++ K_Nearest_Neighbours/knn_sklearn.py
@@ -1,9 +1,8 @@
 import numpy as np
-from sklearn import preprocessing, neighbors
+from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import pickle
-import os
+
 # Load the data set
 df = pd.read_csv('breast-cancer-wisconsin.data.txt')
 
@@ -17,25 +16,10 @@ y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Load Classifier of K Nearest Neighbours
-pickle_file_name = 'knn_sklearn.pickle'
-
-# If pre-trained pickle exists load it
-if os.path.isfile('./' + pickle_file_name):
-    # Load the classifier from the pre-trained pickle
-    pickle_file = open(pickle_file_name, 'rb')
-    clf = pickle.load(pickle_file)
-# Otherwise train the classifier and save it in a pickle
-else:
-    # Default k = 5
-    clf = neighbors.KNeighborsClassifier()
-
-    # train the model on training data
-    clf.fit(X_train, y_train)
-
-    # save the pickle
-    with open(pickle_file_name, 'wb') as f:
-        pickle.dump(clf, f)
+# Default k = 5
+clf = neighbors.KNeighborsClassifier()
+# train the model on training data
+clf.fit(X_train, y_train)
 
 # Get the accuracy on the testing data
 accuracy = clf.score(X_test, y_test)
